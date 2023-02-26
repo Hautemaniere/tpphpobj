318baf6bd61f4d4c67b910cb570cbf04dc3fde71
Hautemaniere <edouard.hte@gmail.com>
2023-02-26 19:05:52 +0100

    exo 4

--- class.php
+++ class.php
@@ -43,3 +43,34 @@ class personnage2{
 
 }
 ?>
+
+<?php
+class personnage3{
+    private $pseudo;
+    private $vie;
+
+    public function __construct($psuedoUser){
+        $this -> vie =  100;
+        $this -> pseudo = $psuedoUser;
+    }
+
+    public function afficheViePseudo(){
+        echo "<br>Je suis  " .$this -> pseudo ." et j'ai : " .$this -> vie. " points de vie";
+    }
+
+    public function affichePseudo(){
+        return $this -> pseudo;
+    }
+
+    public function attaquer($PersoAattaquer){
+       $PersoAattaquer-> defense(50);
+       echo "<br>Je suis ". $this -> pseudo . " et j'attaque ".$PersoAattaquer->affichePseudo();
+    }
+
+    public function defense($ValeurDattaque){
+        $this->vie -= $ValeurDattaque;
+    }
+
+
+}
+?>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <a href="exo1obj.php">Exo1</a>
     <a href="exo2obj.php">Exo2</a>
     <a href="exo3obj.php">Exo3</a>
+    <a href="exo4obj.php">Exo4</a>
 
 
     </body>
